0befb35e789b6031126ef3f26ce266a61df88ee1
jorge-gx <jorge-dev@outlook.com>
2019-04-17 17:24:20 -0500

    update to S6L3 examples

---
+++ sec06_unions_outer_joins/S6L3_SQL_Examples.sql
@@ -0,0 +1,100 @@
+/* OUTER JOINS */
+
+
+select * from rent_cars;
+-- i have four cars that can be rented
+select * from rentals;
+-- i have three occurences of actual rentals
+
+
+---
+-- Inner join of RENT_CARS and RENTALS where VINs match
+---
+SELECT RC.VIN, 
+       RC.MAKE, 
+       RC.MODEL, 
+       RC.MODEL_YEAR, 
+       RC.MILEAGE, 
+       RC.STATE, 
+       RC.TAG, 
+       RC.IN_OUT, 
+       R.RENTAL_ID, 
+       R.DT_OUT, 
+       R.DT_RETURN
+FROM RENT_CARS RC, 
+     RENTALS R
+WHERE RC.VIN = R.VIN
+;
+ 
+---
+-- Left Outer Join 
+---
+SELECT RC.VIN, 
+       RC.MAKE, 
+       RC.MODEL, 
+       RC.MODEL_YEAR, 
+       RC.MILEAGE, 
+       RC.STATE, 
+       RC.TAG, 
+       RC.IN_OUT, 
+       R.RENTAL_ID, 
+       R.DT_OUT, 
+       R.DT_RETURN
+FROM RENT_CARS RC LEFT OUTER JOIN  RENTALS R ON RC.VIN = R.VIN
+;
+
+
+---
+-- Correlated Subquery returns max DT_OUT for each VIN
+---
+SELECT R.VIN, R.RENTAL_ID, R.DT_OUT, R.DT_RETURN
+FROM RENTALS R 
+WHERE R.DT_OUT = (
+                 SELECT MAX(R_DT.DT_OUT)
+                  FROM RENTALS R_DT
+                 WHERE R.VIN = R_DT.VIN
+                )
+;
+
+
+
+---
+-- Using a query as a table with a table alias
+---
+SELECT RQ.VIN, RQ.RENTAL_ID, RQ.DT_OUT, RQ.DT_RETURN FROM 
+(
+  SELECT R.VIN, R.RENTAL_ID, R.DT_OUT, R.DT_RETURN
+  FROM RENTALS R 
+  WHERE R.DT_OUT = (
+                   SELECT MAX(R_DT.DT_OUT)
+                    FROM RENTALS R_DT
+                   WHERE R.VIN = R_DT.VIN
+                  )
+) RQ
+;
+
+---
+-- Left Outer Join using query as Right "table"
+---
+SELECT RC.VIN, 
+       RC.MAKE, 
+       RC.MODEL, 
+       RC.MODEL_YEAR, 
+       RC.MILEAGE, 
+       RC.STATE, 
+       RC.TAG, 
+       RC.IN_OUT, 
+       RQ.RENTAL_ID, 
+       RQ.DT_OUT, 
+       RQ.DT_RETURN
+FROM RENT_CARS RC LEFT OUTER JOIN 
+    (
+      SELECT R.VIN, R.RENTAL_ID, R.DT_OUT, R.DT_RETURN
+      FROM RENTALS R 
+      WHERE R.DT_OUT = (
+                       SELECT MAX(R_DT.DT_OUT)
+                        FROM RENTALS R_DT
+                       WHERE R.VIN = R_DT.VIN
+                      )
+    ) RQ ON RC.VIN = RQ.VIN
+;
